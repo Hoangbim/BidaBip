89c15c12c762291c97de7910fac5063bd27520e9
Hoang <nvhoagnwru@gmail.com>
2023-04-08 05:10:12 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import {
-  RouterProvider,
-  createBrowserRouter,
-  redirect,
-} from "react-router-dom";
+// import {
+//   RouterProvider,
+//   createBrowserRouter,
+//   redirect,
+// } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Layout from "./layout/Layout";
 import ErrorPage from "./pages/ErrorPage";
@@ -10,25 +12,34 @@ import DashBoard from "./pages/dasboard";
 import LoginPage from "./pages/login";
 
 function App() {
-  const route = createBrowserRouter([
-    {
-      // path: "/",
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          element: <LoginPage />,
-        },
-        {
-          path: "/:id",
-          element: <DashBoard />,
-        },
-      ],
-    },
-  ]);
+  // const route = createBrowserRouter([
+  //   {
+  //     // path: "/",
+  //     element: <Layout />,
+  //     errorElement: <ErrorPage />,
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <LoginPage />,
+  //       },
+  //       {
+  //         path: "/:id",
+  //         element: <DashBoard />,
+  //       },
+  //     ],
+  //   },
+  // ]);
 
-  return <RouterProvider router={route} />;
+  // return <RouterProvider router={route} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LoginPage />} />
+        <Route path=":id" element={<DashBoard />} />
+        <Route path="*" element={<div>not found</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
